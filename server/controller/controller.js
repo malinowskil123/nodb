28541e04ca5bde8993281b5c5c01ac902a33bdb5
Lukasz Malinowski <lukaszmalinowski@Lukaszs-MacBook-Pro.local>
2020-02-07 12:16:34 -0700

    finished backend

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -1,4 +1,37 @@
 const list = []
 let id = 0
 
-module.exports = {}
+module.exports = {
+  getTaskList: (req, res) => {
+    res.status(200).send(list)
+  },
+  addTask: (req, res) => {
+    const { text, date, dueDate } = req.body
+    list.push({
+      id: id,
+      date: date,
+      text: text,
+      dueDate: dueDate
+    })
+    id++
+    res.status(200).send(list)
+  },
+  updateTask: (req, res) => {
+    const { id } = req.params
+    const { text, dueDate } = req.body
+    let index = list.findIndex(elm => elm.id === +id)
+    list[index] = {
+      id: list[index].id,
+      date: list[index].date,
+      text: text || list[index].text,
+      dueDate: dueDate || list[index].dueDate
+    }
+    res.status(200).send(list)
+  },
+  deleteTask: (req, res) => {
+    const { id } = req.params
+    let index = list.findIndex(elm => elm.id === +id)
+    list.splice(index, 1)
+    res.status(200).send(list)
+  }
+}
--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const ctrl = require('./controller/controller')
+const baseUrl = require('./resources')
 
 const app = express()
 const PORT = 4040
@@ -9,5 +10,9 @@ app.use(express.json())
 app.use(cors())
 
 //endpoints-----------
+app.get(baseUrl, ctrl.getTaskList)
+app.post(baseUrl, ctrl.addTask)
+app.put(`${baseUrl}/:id`, ctrl.updateTask)
+app.delete(`${baseUrl}/:id`, ctrl.deleteTask)
 
 app.listen(PORT, () => console.log(`server running on port: ${PORT}`))
